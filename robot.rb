a1808b6a574a304eb6fa1ac2c60ac9cee982bd6f
David Mills <dave@1morepx.com>
2018-08-13 15:42:32 -0400

    allow robots to go berserk

--- robot.rb
+++ robot.rb
@@ -1,9 +1,10 @@
 class Robot
   attr_reader :name, :health
+  MAX_HEALTH = 50
 
   def initialize(name, power = 10)
     @name = name
-    @health = 50
+    @health = MAX_HEALTH
     @power = power
   end
 
@@ -11,8 +12,16 @@ class Robot
     @health <= 0
   end
 
+  def berserk?
+    @health <= MAX_HEALTH * 0.2
+  end
+
   def attack(other_robot)
     damage = rand(@power) + 1
+    if berserk?
+      puts "😡 #{name} is GOING BERSERK!"
+      damage *= 2
+    end
 
     puts "#{name} does #{damage} damage to #{other_robot.name}"
     other_robot.take_damage(damage)
@@ -23,6 +32,6 @@ class Robot
   end
 
   def summary
-    "#{name}: #{@health} HP (#{@power} AP)"
+    "#{name}: #{@health} HP (#{@power} AP)#{berserk? ? " Going Berserk!" : ""}"
   end
 end
